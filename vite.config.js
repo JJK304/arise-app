eb6006ec001fefd19f2ed2219ee93bd50cd02030
JJK304 <joris.kandanoleon@gmail.com>
2026-06-04 22:57:29 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icon.svg'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        runtimeCaching: [],
+      },
+      manifest: {
+        name: 'ARISE',
+        short_name: 'ARISE',
+        description: 'Solo Leveling RPG Tracker',
+        theme_color: '#050508',
+        background_color: '#050508',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          { src: 'icon.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any maskable' }
+        ]
+      }
+    })
+  ],
+})
